2aa26883fc678f3c12356bf307c6d6572cf051f8
Nick <nick.ledin@hotmail.com>
2023-09-24 11:56:48 +0200

    TODO

---
+++ src/com/nick/game/Main.java
@@ -0,0 +1,29 @@
+package com.nick.game;
+
+public class Main {
+
+    /*
+    TODO:
+    Antal spelare
+    Antal tärningar
+    Spelares namn
+    Minst 2 spelare
+    Högst poäng vinner
+
+    VG
+    2 vinnare, hantera
+    Scanner i egen klass
+    Hantera att rätt svar kommer in under inmatning
+        Namn får bara vara String
+        Nummer får bara vara integer
+    3 relevanta enhetstester?
+
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
---
+++ src/com/nick/game/UserInput.java
@@ -0,0 +1,5 @@
+package com.nick.game;
+
+public class UserInput {
+    
+}
