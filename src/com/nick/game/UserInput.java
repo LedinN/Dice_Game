8d3f75bb9633eb4420eff96681bf9dbd776443fe
Nick <nick.ledin@hotmail.com>
2023-09-25 07:04:23 +0200

    new scanner class

--- src/com/nick/game/UserInput.java
+++ src/com/nick/game/UserInput.java
@@ -1,5 +1,19 @@
 package com.nick.game;
 
+import java.util.Scanner;
+
 public class UserInput {
-    
+    Scanner sc = new Scanner(System.in);
+
+    // Integer input
+    public int intScanner() {
+
+    return 0; //Placeholder
+    }
+
+    // String input
+    public String stringScanner() {
+
+        return "placeholder";
+    }
 }
