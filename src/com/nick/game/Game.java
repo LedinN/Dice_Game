2382c0957f0a6a43d7347f54ee5eb35d4ae49e3c
Nick <nick.ledin@dinbil.se>
2023-10-02 15:52:25 +0200

    testing arrays

--- src/com/nick/game/Game.java
+++ src/com/nick/game/Game.java
@@ -10,6 +10,8 @@ public class Game {
         // HUR MÅNGA SPELARE
         System.out.println("How many players?");
         int players = new UserInput().intScanner();
+        int[] highscore = new int[players];
+        int sum = 0;
         String[] playernames = new String[players];
 
         for (int i = 0; i < players; i++) {
@@ -18,26 +20,22 @@ public class Game {
             System.out.println("Player "+(i+1)+"'s name is: "+playernames[i]+"\n");
         }
 
-
         System.out.println("How many dices should each player have?");
         int dices = new UserInput().intScanner();
-
 
             for (int j = 0; j < players; j++) {
 
                 System.out.println("It's " + playernames[j] + "s turn!\n Press 'Enter' to roll: ");
 
                 for (int k = 0; k < dices; k++) {
-                    int sum = 0;
 
                     int score = random.nextInt(6) + 1;
                     System.out.println(playernames[j]+" rolled a "+score);
                     sum += score;
                     System.out.println("Total points this round: "+sum);
                     new UserInput().pressEnter();
-
-
                 }
+
             }
 
         }
