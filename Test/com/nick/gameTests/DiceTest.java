6152dfab9d1aeeeb212e10330fc60ea7e02314ea
Nick <nick.ledin@hotmail.com>
2023-10-08 20:00:13 +0200

    Final commit, probably

--- Test/com/nick/gameTests/DiceTest.java
+++ Test/com/nick/gameTests/DiceTest.java
@@ -6,21 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DiceTest {
     @Test
-    public void testRollInRange(){
+    public void testRollInRange() {
 
         for (int i = 0; i < 1000; i++) {
-            int sum = new Engine().diceRoller(1,"Nick" );
+            int sum = new Engine().diceRoller(1, "Nick");
             assertTrue(sum >= 1 && sum <= 6, "Dice roll result out of range: " + sum);
         }
     }
-    @Test
-    public void scannerTest(){
-
-
-
-    }
-
-
-
-
 }
